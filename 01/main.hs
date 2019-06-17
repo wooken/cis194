81ed9c116db67310ea98b14958cc58525483fd30
wooken <kenny@kenwoo.me>
2019-06-16 21:35:00 -0700

    Add week 1

---
+++ 01/main.hs
@@ -0,0 +1,52 @@
+exercise1 = do
+  print "Exercise 1"
+  print $ toDigits 1234 == [1, 2, 3, 4]
+  print $ toDigitsRev 1234 == [4, 3, 2, 1]
+  print $ toDigits 0 == []
+  print $ toDigits (-17) == []
+
+toDigits :: Integer -> [Integer]
+toDigits x
+  | x > 0 = toDigits (x `div` 10) ++ [x `mod` 10]
+  | otherwise = []
+
+toDigitsRev :: Integer -> [Integer]
+toDigitsRev x
+  | x > 0 = x `mod` 10 : toDigitsRev (x `div` 10)
+  | otherwise = []
+
+exercise2 = do
+  print "Exercise 2"
+  print $ doubleEveryOther [8, 7, 6 ,5] == [16, 7, 12, 5]
+  print $ doubleEveryOther [1, 2, 3] == [1, 4, 3]
+
+doubleEveryOtherFromLeft :: [Integer] -> [Integer]
+doubleEveryOtherFromLeft [] = []
+doubleEveryOtherFromLeft [x] = [x]
+doubleEveryOtherFromLeft [x, y] = [x, 2 * y]
+doubleEveryOtherFromLeft [x, y, z] = [x, 2 * y, z]
+doubleEveryOtherFromLeft (x:y:zs) = [x, 2* y] ++ doubleEveryOtherFromLeft zs
+
+doubleEveryOther :: [Integer] -> [Integer]
+doubleEveryOther = reverse . doubleEveryOtherFromLeft . reverse
+
+exercise3 = do
+  print "Exercise 3"
+  print $ sumDigits [16, 7, 12, 5] == 22
+
+sumDigits :: [Integer] -> Integer
+sumDigits = sum . map (sum . toDigits)
+
+exercise4 = do
+  print "Exercise 4"
+  print $ validate 4012888888881881 == True
+  print $ validate 4012888888881882 == False
+
+validate :: Integer -> Bool
+validate x = (sumDigits . doubleEveryOther . toDigits $ x) `mod` 10 == 0
+
+main = do
+  exercise1
+  exercise2
+  exercise3
+  exercise4
